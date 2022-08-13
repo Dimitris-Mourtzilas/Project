e97d6982c79bcce26975b9db0ba4dc2bd05f9cbb
Dimitris-Mourtzilas <dimitris.mourtzilas1999@gmail.com>
2022-08-13 10:43:56 +0200

    created database plus controllers

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -5,12 +5,11 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\UserFormType;
 use App\Repository\UserRepository;
-use Cassandra\Type\UserType;
 use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\Form\FormBuilder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PropertyAccess\PropertyAccess;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -35,26 +34,21 @@ class BaseController extends AbstractController
      *
      * @return Response
      */
-    public function addUser(ManagerRegistry $reg, Request $request): Response
+    public function addUser(ManagerRegistry $reg, Request $request):Response
     {
 
         $user = new User();
-        $form = $this->createForm(UserFormType::class, $user);
 
         if ($request->getMethod() == "GET") {
-            return $this->renderForm('user/createUser.html.twig', ['form' => $form]);
-
+            return $this->render('user/createUser.html.twig');
         } else {
 
-            $form->handleRequest($request);
-
-            if ($form->isValid() && $form->isSubmitted()) {
-
-                $task = $form->getData();
-                var_dump($task);
-                $user->setFirstName($task['firstName']);
-            }
+            $user->setFirstName($request->request->get('firstName'));
+            $userRepo = new UserRepository($reg);
+                $userRepo->add($user);
+                return $this->redirect("/");
 
         }
+
     }
 }
